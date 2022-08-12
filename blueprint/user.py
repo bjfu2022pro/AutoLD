b2d6167c25c24021f512f1a9daa0c748f68205ab
zhenhualian <bjfueleclian@qq.com>
2022-08-12 21:56:55 +0800

    -add email func

--- blueprint/user.py
+++ blueprint/user.py
@@ -2,9 +2,14 @@ from flask import Blueprint, render_template, request
 import sys 
 sys.path.append("..") 
 import util_user
+import util_email
+from flask_mail import Mail, Message
+import random
+import string
+
 
 bp = Blueprint("user", __name__, "/")
-
+mail = Mail()
 
 @bp.route('/')
 @bp.route('/login', methods=['get', 'post']) 
@@ -47,3 +52,18 @@ def user_control():
 @bp.route('/password_forget')
 def password_forget():
     return render_template("password_forget.html")
+
+
+@bp.route('/mail')
+def send_vcode():
+    email = request.args.get("email")
+    vcode = "".join(random.sample(string.digits, 4))
+    mail_body = "您的验证码是：{}".format(vcode)
+    message = Message(
+        subject="test",
+        recipients=[email],
+        body=mail_body
+    )
+    mail.send(message)
+    util_email.captcha_insert(email, vcode)
+    return "success"
