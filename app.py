f61338f004ca220570bee7d85ae07406b26deb8c
zhenhualian <bjfueleclian@qq.com>
2022-08-12 17:00:33 +0800

    -blueprint分级

--- app.py
+++ app.py
@@ -1,52 +1,9 @@
 from flask import Flask
-from flask import request
-from flask import render_template
-import util_user
+from blueprint import user_bp
 
 app = Flask(__name__)
 
-
-@app.route('/')
-@app.route('/login', methods=['get', 'post'])
-def login():
-    return render_template("login.html")
-
-
-@app.route('/login_result', methods=['get', 'post'])
-def login_checker():
-    username = request.values.get('phonenumber')
-    password = request.values.get('password')
-    info = util_user.login_check(username, password)
-    print(info)
-    return info
-
-
-@app.route('/register')
-def register():
-    return render_template("register.html")
-
-
-@app.route('/reg_result', methods=['get', 'post'])
-def register_check():
-    username = request.values.get('username')
-    vcode = request.values.get('vcode')
-    password = str(request.values.get('password'))
-    repwd = str(request.values.get('repwd'))
-    if password != repwd:
-        return "两次密码不一致"
-    else:
-        info = util_user.add_user(username, password)
-        return info
-
-
-@app.route('/usercontrol')
-def user_control():
-    return render_template("usercontrol.html")
-
-
-@app.route('/password_forget')
-def password_forget():
-    return render_template("password_forget.html")
+app.register_blueprint(user_bp)
 
 
 if __name__ == '__main__':
---
+++ blueprint/user.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template, request
+import sys 
+sys.path.append("..") 
+import util_user
+
+bp = Blueprint("user", __name__, "/user")
+
+
+@bp.route('/')
+@bp.route('/login', methods=['get', 'post']) #127.0.0.1:5000/user/login
+def login():
+    return render_template("login.html")
+
+
+@bp.route('/login_result', methods=['get', 'post'])
+def login_checker():
+    username = request.values.get('phonenumber')
+    password = request.values.get('password')
+    info = util_user.login_check(username, password)
+    print(info)
+    return info
+
+
+@bp.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@bp.route('/reg_result', methods=['get', 'post'])
+def register_check():
+    username = request.values.get('username')
+    vcode = request.values.get('vcode')
+    password = str(request.values.get('password'))
+    repwd = str(request.values.get('repwd'))
+    if password != repwd:
+        return "两次密码不一致"
+    else:
+        info = util_user.add_user(username, password)
+        return info
+
+
+@bp.route('/usercontrol')
+def user_control():
+    return render_template("usercontrol.html")
+
+
+@bp.route('/password_forget')
+def password_forget():
+    return render_template("password_forget.html")
