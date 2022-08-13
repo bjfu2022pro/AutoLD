2b655bb6e5a35a7c4e5dc80303ec999fbe37743d
zhenhualian <1109822790@qq.com>
2022-08-13 19:22:47 +0800

    -fix bug

--- blueprint/mall.py
+++ blueprint/mall.py
@@ -10,15 +10,15 @@ import util_calculate_mall
 bp = Blueprint("mall", __name__, "/")
 
 
-AI_select=util_AIgorithmic_mall.find_all()
-calculate_select=util_calculate_mall.find_all()
 
 @bp.route('/AIgorithmic_mall', methods=['get', 'post'])
 def AIgorithmic():
+    AI_select=util_AIgorithmic_mall.find_all()
     return render_template("AIgorithmic_mall.html", AI_select=AI_select)
 
 
 @bp.route('/calculate_mall', methods=['get', 'post'])
 def calculate():
+    calculate_select=util_calculate_mall.find_all()
     return render_template("calculate_mall.html",calculate_select=calculate_select)
 
