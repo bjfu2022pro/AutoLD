c26bd68cec40a164541e37216be397104dc8ea90
DengFei71771 <2465968689@qq.com>
2022-08-13 14:54:44 +0800

    0813邓飞

--- blueprint/mall.py
+++ blueprint/mall.py
@@ -1,1 +1,24 @@
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, render_template, request
+
+from flask import Flask
+import sys
+sys.path.append("..")
+import util_AIgorithmic_mall
+import util_calculate_mall
+
+
+bp = Blueprint("mall", __name__, "/")
+
+
+AI_select=util_AIgorithmic_mall.find_all()
+calculate_select=util_calculate_mall.find_all()
+
+@bp.route('/AIgorithmic_mall', methods=['get', 'post'])
+def AIgorithmic():
+    return render_template("AIgorithmic_mall.html", AI_select=AI_select)
+
+
+@bp.route('/calculate_mall', methods=['get', 'post'])
+def calculate():
+    return render_template("calculate_mall.html",calculate_select=calculate_select)
+
