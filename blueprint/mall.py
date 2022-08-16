4864c3e2322234b8672045caf0d856f2c5332e3f
maxparkjxg <914177457@qq.com>
2022-08-16 23:38:17 +0800

    jin

--- blueprint/mall.py
+++ blueprint/mall.py
@@ -1,22 +1,25 @@
 from flask import Blueprint, render_template, request
-import util_user,util_pay_confirm
+import util_user, util_pay
 from flask import Flask
 import sys
+from flask import g
 
 sys.path.append("..")
 import util_algorithmic_mall
 import util_calculate_mall
+import util_pay
 
 bp = Blueprint("mall", __name__, "/")
 
 dingdan = [
-     {
-         'dingdanhao': 1000, 'peizhi': '空',
-         'leixing': '空', 'shujuji': '空',
-         'jine': '空', 'shijian': '空',
-         'fukuanren': '大公园'
-     }
- ]
+    {
+        'dingdanhao': 1000, 'peizhi': '空',
+        'leixing': '空', 'shujuji': '空',
+        'jine': '空', 'shijian': '空',
+        'fukuanren': '大公园'
+    }
+]
+
 
 @bp.route('/algorithmic_mall', methods=['get', 'post'])
 def algorithmic():
@@ -33,6 +36,12 @@ def calculate():
 
 @bp.route('/pay')
 def confirm():
-    return render_template('pay.html',dingdan=dingdan)
+    return render_template('pay.html', dingdan=dingdan)
 
 
+@bp.route('/my_bill')
+def my_bill():
+    email = g.info[1]
+    print(email)
+    my_bill =util_pay.show_orders(email)
+    return render_template('my_bill.html',my_bill=my_bill)
