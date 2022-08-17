2c0ccdf926b6c9c637ccc3c1721e6b3fac724ff5
DengFei71771 <2465968689@qq.com>
2022-08-17 11:06:34 +0800

    0817邓飞1106

--- blueprint/mall.py
+++ blueprint/mall.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 
 from flask import Flask
 import sys
@@ -32,7 +32,27 @@ def calculate():
     calculate_select=util_calculate_mall.find_all()
     return render_template("calculate_mall.html",calculate_select=calculate_select)
 
+@bp.route('/cache', methods=['get', 'post'])
+def cache():
+    # title = request.values.get("title")
+    # introduce = request.values.get("introduce")
+    # print(title,introduce)
+    # util_cache.add(title=title,introduce=introduce)
+    # util_algorithmic_mall.find_all()
+    id = int(request.values.get("id"))
+    print(id)
+    details = util_algorithmic_mall.finder(id)
+    print(details)
+    util_cache.add(details[0][1], details[0][2])
+    return jsonify({"code": 200})
 
+
+@bp.route('/algorithmic_details', methods=['get', 'post'])
+def details():
+    details = util_cache.find_all()
+    print(details)
+    util_cache.delete(details[0][0])
+    return render_template("algorithmic_details.html", details=details)
 
 
 @bp.route('/pay')
