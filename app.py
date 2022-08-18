b02e116ad9fa5ad95704ad80f14dd7fc36d76287
zhenhualian <1109822790@qq.com>
2022-08-18 14:25:34 +0800

    -添加两个函数和一个bp

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, session, g
-from blueprint import user_bp, mall_bp
+from blueprint import user_bp, mall_bp, in_bp
 
 import config
 from flask_mail import Mail
@@ -13,6 +13,7 @@ mail.init_app(app)
 
 app.register_blueprint(user_bp)
 app.register_blueprint(mall_bp)
+app.register_blueprint(in_bp)
 
 
 @app.before_request
