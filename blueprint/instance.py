2d87f6ebde85eae537804b13eaa1ee18c670fd99
zhenhualian <1109822790@qq.com>
2022-08-18 16:09:26 +0800

    -呜呜呜，跑起来了

--- blueprint/instance.py
+++ blueprint/instance.py
@@ -4,7 +4,7 @@ from flask import (Blueprint, jsonify,
                     redirect, g)
 import threading
 import sys
-import algorithm
+from algorithm import cls, regress
 
 import datetime
 
@@ -19,19 +19,23 @@ bp = Blueprint("instance", __name__, "/")
 @bp.route('/run_instance', methods=['post', 'get'])
 def run_instance():
     in_id = request.args.get('id')
-    ntime = str(datetime.datetime.now()).split(".")[0]
-    ntime = datetime.datetime.strptime(ntime, '%Y-%m-%d %H:%M:%S')
-    util_instance.update_info(in_id, ntime, 'begin_time')
-    util_instance.update_info(in_id, 2, 'state')
+
     result = util_instance.find_instance_byid(in_id)
     email = result[0][1]
     al_id = result[0][2]
-    data_path = util_dataset.finde_dataset(result[0][3])
-    if al_id == 1:
-        t0 = threading.Thread(target=algorithm.regression.regress, args=(data_path, email, result[0][0]))
-        t0.start()
-    elif al_id == 2:
-        t0 = threading.Thread(target=algorithm.cn.cls, args=(data_path, email, result[0][0]))
-        t0.start()
+    data = util_dataset.finde_dataset(result[0][3])
+    data_path = data[0][4]
+
+    ntime = str(datetime.datetime.now()).split(".")[0]
+    ntime = datetime.datetime.strptime(ntime, '%Y-%m-%d %H:%M:%S')
+    util_instance.update_info(in_id, ntime, 'begin_time')
+    util_instance.update_info(in_id, 1, 'state')
+
+    t0 = threading.Thread()
+    if al_id == '1':
+        t0 = threading.Thread(target=regress, args=(data_path, email, result[0][0]))  
+    elif al_id == '2':
+        t0 = threading.Thread(target=cls, args=(data_path, email, result[0][0]))
+    t0.start()
     return redirect('/my_instance')
 
