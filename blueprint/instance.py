8b08e5ca7cd2018392d0505db0cc02d18f4db5c3
zhenhualian <1109822790@qq.com>
2022-08-19 12:00:00 +0800

    -state code

--- blueprint/instance.py
+++ blueprint/instance.py
@@ -18,24 +18,30 @@ bp = Blueprint("instance", __name__, "/")
 
 @bp.route('/run_instance', methods=['post', 'get'])
 def run_instance():
-    in_id = request.args.get('id')
+    in_id = request.values.get('id')
 
     result = util_instance.find_instance_byid(in_id)
     email = result[0][1]
     al_id = result[0][2]
     data = util_dataset.finde_dataset(result[0][3])
     data_path = data[0][4]
+    state = result[0][8]
+    if state == '0':
+        ntime = str(datetime.datetime.now()).split(".")[0]
+        ntime = datetime.datetime.strptime(ntime, '%Y-%m-%d %H:%M:%S')
+        util_instance.update_info(in_id, ntime, 'begin_time')
+        util_instance.update_info(in_id, 1, 'state')
 
-    ntime = str(datetime.datetime.now()).split(".")[0]
-    ntime = datetime.datetime.strptime(ntime, '%Y-%m-%d %H:%M:%S')
-    util_instance.update_info(in_id, ntime, 'begin_time')
-    util_instance.update_info(in_id, 1, 'state')
-
-    t0 = threading.Thread()
-    if al_id == '1':
-        t0 = threading.Thread(target=regress, args=(data_path, email, result[0][0]))  
-    elif al_id == '2':
-        t0 = threading.Thread(target=cls, args=(data_path, email, result[0][0]))
-    t0.start()
-    return redirect('/my_instance')
-
+        t0 = threading.Thread()
+        if al_id == '1':
+            t0 = threading.Thread(target=regress, args=(data_path, email, result[0][0]))  
+        elif al_id == '2':
+            t0 = threading.Thread(target=cls, args=(data_path, email, result[0][0]))
+        t0.start()
+        return jsonify({'code':200})
+    elif state == '1':
+        return jsonify({'code':300})
+    elif state == '2':
+        return jsonify({'code':400})
+    else:
+        return jsonify({'code':500})
