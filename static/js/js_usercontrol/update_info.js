d0c436e5d1b8690529bfdbd1a11013daf24fc31f
sunyxah <1922587835@qq.com>
2022-08-23 16:23:46 +0800

    管理员登录 更改密码 忘记密码 更新个人信息

--- static/js/js_usercontrol/update_info.js
+++ static/js/js_usercontrol/update_info.js
@@ -1,3 +1,25 @@
+var sqlKeyWords = "select ,union ,asc ,desc ,in ,like ,into ,exec ,from ";
+sqlKeyWords += ",update ,insert ,delete ,count ,asc( ,char( ,chr( ,drop ,table ,truncat ";
+sqlKeyWords += ",mid( ,abs( ,= ,-- ,<script ,/script ";
+sqlKeyWords += ",where ,join ,create ,alter ,cast ,exists ,; , or , and ,order by ,group by ";
+//分割成数组
+var sqls = sqlKeyWords.split(",");
+
+
+function checkSqlInj(testInput) {                           //检测输入是否有sql关键词，有返回true,实际应用见108行
+	var invalid = false;
+	var chkInput = (testInput + "").toLowerCase();
+	var pos = -1;
+	for (var i = 0, n = sqls .length; i < n; i++) {
+		pos = chkInput.indexOf(sqls [i]);
+		if (pos != -1) {
+			invalid = true;
+			break;
+		}
+	}
+	return invalid;
+}
+
 function updateBtn() {
     $("#update_btn").on("click", function (event) {
         var email = $("input[id='email']").val();
@@ -6,6 +28,16 @@ function updateBtn() {
         var company = $("input[id='company']").val();
         var grjm = $("input[id='grjm']").val();
         var describe = $("textarea[id='describe']").val();
+        if(checkSqlInj(email) || checkSqlInj(username) || checkSqlInj(gender) || checkSqlInj(company) || checkSqlInj(grjm) || checkSqlInj(describe)){
+            document.getElementById('email').value = '';
+            document.getElementById('username').value = '';
+            document.getElementById('gender').value = '';
+            document.getElementById('company').value = '';
+            document.getElementById('grjm').value = '';
+            document.getElementById('describe').value = '';
+            alert("你的输入中有敏感词！请重新输入！");
+            return;
+        }
         $.ajax({
             url: "/user_update",
             method: "get",
